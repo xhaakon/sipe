d316176b74a4697a41320d0b999a565cf970e85c
Youness Alaoui <kakaroto@kakaroto.homelinux.net>
2014-08-12 09:22:47 +0200

    media: add extra SDP for file transfer/desktop sharing

--- src/core/sdpmsg.c
+++ src/core/sdpmsg.c
@@ -670,6 +670,7 @@ media_to_string(const struct sdpmsg *msg, const struct sdpmedia *media)
 	gchar *crypto = NULL;
 
 	gboolean uses_tcp_transport = FALSE;
+	gchar *extra_sdp = NULL;
 
 	if (media->port != 0) {
 		if (!sipe_strequal(msg->ip, media->ip)) {
@@ -708,11 +709,21 @@ media_to_string(const struct sdpmsg *msg, const struct sdpmedia *media)
 		}
 	}
 
+	if (sipe_strequal(media->name, "applicationsharing")) {
+		extra_sdp = g_strdup_printf("a=x-applicationsharing-session-id:1\r\n"
+					    "a=x-applicationsharing-role:viewer\r\n"
+					    "a=x-applicationsharing-media-type:rdp\r\n");        
+	} else if (sipe_strequal(media->name, "data")) {
+		extra_sdp = g_strdup_printf("a=mid:1\r\n"
+					    "a=recvonly\r\n");        
+	}
+
 	transport_profile = g_strdup_printf("%sRTP/%sAVP",
 					    uses_tcp_transport ? "TCP/" : "",
 					    media->encryption_active ? "S" : "");
 
 	media_str = g_strdup_printf("m=%s %d %s%s\r\n"
+				    "%s"
 				    "%s"
 				    "%s"
 				    "%s"
@@ -727,7 +738,8 @@ media_to_string(const struct sdpmsg *msg, const struct sdpmedia *media)
 				    remote_candidates_str ? remote_candidates_str : "",
 				    codecs_str ? codecs_str : "",
 				    attributes_str ? attributes_str : "",
-				    credentials ? credentials : "");
+				    credentials ? credentials : "",
+				    extra_sdp ? extra_sdp : "");
 
 	g_free(transport_profile);
 	g_free(media_conninfo);
@@ -738,6 +750,8 @@ media_to_string(const struct sdpmsg *msg, const struct sdpmedia *media)
 	g_free(attributes_str);
 	g_free(credentials);
 	g_free(crypto);
+	g_free(extra_sdp);
+
 
 	return media_str;
 }
