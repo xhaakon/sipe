158a3309d58b0ef660059ca34b6dfce30eed8bb3
Jakub Adam <jakub.adam@ktknet.cz>
2014-09-04 09:52:54 +0200

    filetransfer: initialize media session for incoming data

--- src/core/sipe-media.c
+++ src/core/sipe-media.c
@@ -268,6 +268,8 @@ backend_stream_to_sdpmedia(struct sipe_backend_media *backend_media,
 		type = SIPE_MEDIA_AUDIO;
 	else if (sipe_strequal(media->name, "video"))
 		type = SIPE_MEDIA_VIDEO;
+	else if (sipe_strequal(media->name, "data"))
+		type = SIPE_MEDIA_APPLICATION;
 	else if (sipe_strequal(media->name, "applicationsharing"))
 		type = SIPE_MEDIA_APPLICATION;
 	else {
@@ -1154,6 +1156,8 @@ process_incoming_invite_call(struct sipe_core_private *sipe_private,
 				type = SIPE_MEDIA_AUDIO;
 			else if (sipe_strequal(id, "video"))
 				type = SIPE_MEDIA_VIDEO;
+			else if (sipe_strequal(id, "data"))
+				type = SIPE_MEDIA_APPLICATION;
 			else if (sipe_strequal(id, "applicationsharing"))
 				type = SIPE_MEDIA_APPLICATION;
 			else
