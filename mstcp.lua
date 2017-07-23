74be27227885f3155a6853a786c5b2887cb53350
Jakub Adam <jakub.adam@ktknet.cz>
2017-07-23 17:11:34 +0200

    Wireshark dissector

---
+++ mstcp.lua
@@ -0,0 +1,44 @@
+local proto = Proto("MSTCP", "MS TCP")
+proto.fields.framinglen = ProtoField.uint16("MSTCP.framinglen", "TCP frame length")
+proto.fields.data = ProtoField.bytes("MSTCP.data", "Data")
+
+function dissectOne(tvbuffer, pinfo, treeitem)
+  local bufferBytesLeft = (tvbuffer:len() - pinfo.desegment_offset)
+
+  if bufferBytesLeft < 2 then
+    -- Incomplete message, we don't even have the TCP framing bytes
+    pinfo.desegment_len = 2 - bufferBytesLeft
+    return false
+  end
+
+  local frameLenBuffer = tvbuffer(pinfo.desegment_offset, 2)
+  local frameLen = frameLenBuffer:uint()
+  bufferBytesLeft = bufferBytesLeft - 2
+
+  if bufferBytesLeft < frameLen then
+    pinfo.desegment_len = frameLen - bufferBytesLeft
+    return false
+  end
+
+  local subtreeitem = treeitem:add(proto, tvbuffer)
+  subtreeitem:add(proto.fields.framinglen, frameLenBuffer)
+  subtreeitem:add(proto.fields.data, tvbuffer(pinfo.desegment_offset + 2, frameLen))
+  bufferBytesLeft = bufferBytesLeft - frameLen
+
+  if bufferBytesLeft == 0 then
+    -- Reading finished successfully 
+    pinfo.desegment_offset = 0
+    return false
+  end
+
+  pinfo.desegment_offset = tvbuffer:len() - bufferBytesLeft
+  return true
+end
+
+function proto.dissector(tvbuffer, pinfo, treeitem)
+  pinfo.cols.protocol = "MS TCP"
+  while dissectOne(tvbuffer, pinfo, treeitem) do
+  end
+end
+
+DissectorTable.get("tcp.port"):add(26129, proto)
